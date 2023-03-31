e8bb20a8ccf97f767ce623bc5126079ccf4dc54e
shrijanlakhey <lakheyshrijan@gmail.com>
2023-03-31 22:00:37 +0545

    changed name of database again

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,7 @@
 <?php
     $host = "127.0.0.1";
     $user = "root";
-    $db = "schedule";
+    $db = "routine";
     $pass = "";
     $dsn = "mysql:host=$host;dbname=$db;";
     $pdo = new PDO($dsn, $user, $pass);  
