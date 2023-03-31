be05d32bf9e553e8193ed592745a546363957a30
Kanak-dev <kanak24.fv@gmail.com>
2023-03-31 22:54:30 +0545

    routine table added

---
+++ routine.php
@@ -0,0 +1,38 @@
+<?php 
+    include_once "./dbconn.php";
+    $query = "SELECT * FROM routine";
+    $stmt = $pdo -> prepare($query); 
+    $stmt -> execute();
+    $periods = $stmt -> fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Routine</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Routine</h2>
+    <table>
+        <tr>
+            <th>Period</th>
+            <th>Subject Name</th>
+            <th>Teacher</th>
+            <th>Time</th>
+        </tr>
+        <?php foreach ($periods as $p) { 
+        ?>
+            <tr>
+                <td><?php echo $p['Period']?></td>
+                <td><?php echo $p['Subject_name']?></td>
+                <td><?php echo $p['Teacher']?></td>
+                <td><?php echo $p['Duration']?></td>
+                <?php } ?>
+            </tr>
+    </table>
+</body>
+</html>
