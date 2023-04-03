a8dc4a7ffd9c1ca574837c6e46cd1895904f11fe
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-03 19:56:33 +0545

    reduced redirect time

--- edit.php
+++ edit.php
@@ -18,7 +18,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="2; url='./routine.php'">
+    <meta http-equiv="refresh" content="0.5; url='./routine.php'">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update</title>
 </head>
