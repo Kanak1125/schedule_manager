6a094c0206a16645b413463418df62bb67f000cf
Kanak-dev <kanak24.fv@gmail.com>
2023-04-05 19:54:01 +0545

    db name changed to schedule_manager

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,7 @@
 <?php
     $host = "127.0.0.1";
     $user = "root";
-    $db = "routine";
+    $db = "schedule_manager";
     $pass = "";
     $dsn = "mysql:host=$host;dbname=$db;";
     $pdo = new PDO($dsn,$user,$pass);  
