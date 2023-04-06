70935fa4f43f5b0ef6240aedd033440302f0c9b0
Kanak-dev <kanak24.fv@gmail.com>
2023-04-06 17:14:58 +0545

    css modified

--- routine.php
+++ routine.php
@@ -19,6 +19,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
         th {
             background-color: #131313;
             color: #d0d0d0;
