a90e9371733cb7f97722175ddb4e8f67cc209601
Raj Pote <rrajpote@gmail.com>
2023-04-06 20:30:13 +0545

    css updated

--- delete.php
+++ delete.php
@@ -6,14 +6,34 @@
     <!-- <meta http-equiv="refresh" content="1.5; url='./routine.php'"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete row</title>
+    <link rel="stylesheet" href=".\css\style.css">
+    <style>
+        body{
+            background-color: black;
+        }
+        form{
+            height: 123px;
+            width: 400px;
+            font-size: 2rem;
+            text-align: center;
+            border: 1px solid black;
+            background-color: aqua;
+            justify-content: center;
+            box-sizing: border-box;
+            align-items: center;
+
+            
+        }
+    </style>
 </head>
+
 
 <body>
     <form action="#" method="post">
-        <label for="search">Search By Id:</label>
+        <label for="search">Search By Id:</label><br>
         <input type="number" name="search" id="search" value="0">
         <a href="routine.php">
-            <input type="submit" value="Delete">
+            <input type="submit" value="Delete" class="btn-del">
         </a>
     </form>
 </body>
--- form.php
+++ form.php
@@ -7,14 +7,37 @@
     <title>Document</title>
     <link rel="stylesheet" href=".\css\style.css">
 </head>
+<style>
+    body{
+        background-color: black;
+    }
+    .form{
+        width: 400px;
+        height: 400px;
+        box-sizing: border-box;
+        font-size: 1rem;
+        padding: 2rem;
+        background-color: aqua;
+        display: grid;
+        border: 2px solid green;
+        justify-content: center;
+        align-items: center;
+    }
+    .btn{
+      width: 200px;
+      padding: 12px;
+      justify-content: center;
+      align-items: center;
+    }
+</style>
 
 <body>
     <?php
         // we use 'GET' method here because we are reading the period data directly from the URL...
         $period_id = $_GET['period'];
     ?>
-    <fieldset>
-        <form action= "" method = "post">
+    
+        <form action= "" method = "post" class="form">
             
             <h2>Manage Your Schedule</h2>
             <label for="period">Period</label><br>   
@@ -29,13 +52,13 @@
                 <label for="e_time">End Time</label><br>
                 <input type="time" name="e_time" id="e_time" required><br>
             </div>
-            <div class="btn1">
+            <div class="btn">
             <input type="submit" name="add" class="btn-add" value="Add" formaction="add.php">
             <input type="submit" name="edit" class="btn-edit" value="Edit" formaction="edit.php">   
             <input type="submit" name="delete" class="btn-del" value="Delete" formaction="delete.php" formnovalidate>
             <!--  formnovalidate attribute added to bypass the form validation only for delete button... -->
             </div>   
         </form>
-    </fieldset>
+    
 </body>
 </html>
