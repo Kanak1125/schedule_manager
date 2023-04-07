543dac8863a1af192daa757308040522b695c22c
Raj Pote <rrajpote@gmail.com>
2023-04-07 08:48:11 +0545

    login page

--- login.php
+++ login.php
@@ -70,21 +70,4 @@
 </div>
 </body>
 </html>
-=======
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
-  </head>
-  <body>
-    <form>
-      <label for="username">Username</label><br />
-      <input type="text" name="username" /><br /><br />
-      <label for="password">Password</label><br />
-      <input type="password" name="password" /><br /><br />
-      <input type="submit" value="Login" /><br />
-    </form>
-  </body>
-</html>
->>>>>>> 380e111b0c66db23d71ddd39516f775d96ca0101
+
