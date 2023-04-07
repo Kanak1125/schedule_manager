6c7a35ee727b36985dbad2ca8f2cfcbd347a29c9
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-07 21:26:27 +0545

    used argon2 for password hashing

--- signup.php
+++ signup.php
@@ -2,9 +2,10 @@
     include_once ("dbconn.php");
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $query = "INSERT INTO users(Username,Password) Values(:username, :password)";
+    $hash_pass = password_hash($password,PASSWORD_ARGON2ID);
+    $query = "INSERT INTO users(Username,Password) Values(:username, :hash_pass)";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':hash_pass', $hash_pass);
     $stmt->execute();
 ?>
